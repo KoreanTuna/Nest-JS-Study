fb750bad2d387d5aeaf1fd978d84d4e798d5bd04
KoreanTuna <songsee0528@gmail.com>
2025-07-12 02:41:26 +0900

    엔드포인트 제작

--- plain_node_js/1_server.js
+++ plain_node_js/1_server.js
@@ -1,14 +1,34 @@
 const http = require("http");
+const url = require("url");
 
 /// 127.0.0.1 -> loop back
 const host = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
-  res.end("<h1>Hello, World!</h1>");
+  const path = url.parse(req.url).pathname;
+
+  if (path === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end("<h1>Home Page</h1>");
+  } else if (path === "/post") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end("<h1>Post Page</h1>");
+  } else if (path === "/about") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end("<h1>About Page</h1>");
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+    });
+    res.end("<h1>404 Not Found</h1>");
+  }
 });
 
 server.listen(port, host, () => {
