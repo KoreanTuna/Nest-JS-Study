eda22a6ac1dc1d5b635819811a0232e9889bfb44
KoreanTuna <songsee0528@gmail.com>
2025-07-12 11:42:05 +0900

    Post 모듈 생성

---
+++ nestjs_server/src/posts/posts.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+/**
+ * author : string;
+ * title : string;
+ * content : string;
+ * likeCount : number;
+ * commentCount : number;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'Minwoo',
+      title: 'Happy to learn NestJS',
+      content: 'NestJS 맛보기',
+      likeCount: 10000,
+      commentCount: 10,
+    };
+  }
+}
